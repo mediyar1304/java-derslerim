871c3c20ce7cac8495db87b2cff9d97f29039d9e
mediyar1304 <mediyardeniz.1304@gmail.com>
2021-09-05 16:34:01 +0300

    ilk commit

--- src/deneme9.java
+++ src/deneme9.java
@@ -6,8 +6,23 @@ public class deneme9 {
 		
 		// ATM Örnegi
 		
-		Scanner scan = new Scanner(System.in); 
+		Scanner scan = new Scanner(System.in);
+		int bakiye = 1000;
+		int islem;
+		
+	
+		System.out.println("1.Bakiye Sorgulama");
+		System.out.println("2.Para Yatýrma");
+		System.out.println("3.Para Çekme");
+		System.out.println("4.Sistemden Çýkýþ");
+		
+		islem = scan.nextInt();
 		
 		
+	switch(islem) {
+	case 1:
+		
+	
+	}
 	}
 }
