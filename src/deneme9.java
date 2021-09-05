2aff562ea6f80089185da3dc3998a3058552ea7d
mediyar1304 <mediyardeniz.1304@gmail.com>
2021-09-05 16:50:39 +0300

    yeni commit

--- src/deneme9.java
+++ src/deneme9.java
@@ -14,8 +14,7 @@ public class deneme9 {
 		System.out.println("1.Bakiye Sorgulama");
 		System.out.println("2.Para Yatýrma");
 		System.out.println("3.Para Çekme");
-		System.out.println("4.Sistemden Çýkýþ");
-		System.out.println("4.Sistemden Çýkýþ");
+		System.out.println("4.Sistemden Çýkýþ"); 
 		
 		islem = scan.nextInt();
 		
