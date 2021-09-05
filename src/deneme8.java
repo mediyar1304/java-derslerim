610423d7338cedcc333a01dc3cd5fa552c3797e7
navrez2113 <deniznevrez@gmail.com>
2021-09-05 15:48:34 +0300

    ilk commit

---
+++ src/deneme8.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class deneme8 {
+
+	public static void main (String[] args) { 
+ // Mantýksal Koþul
+ String sys_kul_adý = "yazýlýmbilimi";
+ String parola = "12345";
+ 
+ Scanner scan = new Scanner(System.in);
+ System.out.println("Kullanýcý Adý:");
+ 		
+ String kullanýcý_adý = scan.nextLine();
+ System.out.println("Parola");
+ 
+ String kul_parola = scan.nextLine();
+ 
+ if  (!(sys_kul_adý.equals(kullanýcý_adý)) && !(parola.equals(kul_parola)))
+ {
+ System.out.println("Kullanýcý Adý ve Parola Yanlýþ");
+ }
+ else   if  
+	(sys_kul_adý.equals(kullanýcý_adý) && !(parola.equals(kul_parola)))
+ {
+ System.out.println("Parola Yanlýþ");
+}
+ else if  (!(sys_kul_adý.equals(kullanýcý_adý)) && (parola.equals(kul_parola))) 
+ {
+ System.out.println("Kullanýcý Adý Yanlýþ");
+}
+ else  {
+	System.out.println("Giriþ Baþarýlý...");
+}
+
+ }
+}
---
+++ src/deneme9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class deneme9 {
+
+	public static void main(String[] args) {
+		
+		// ATM Örnegi
+		
+		Scanner scan = new Scanner(System.in); 
+		
+		
+	}
+}
